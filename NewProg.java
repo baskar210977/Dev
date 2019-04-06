a7bec06e7bf4e4043a5d614cc37a534982b85963
Baskar <baskar.subbian1@gmail.com>
2019-04-06 17:02:42 +0530

    New Program Commit

---
+++ NewProg.java
@@ -0,0 +1,13 @@
+/* This is a simple Java program. 
+   FileName : "HelloWorld.java". */
+class NewProg
+{ 
+    // Your program begins with a call to main(). 
+    // Prints "Hello, World" to the terminal window. 
+    public static void main(String args[]) 
+    { 
+        System.out.println("New Program Commit..."); 
+    } 
+} 
+
+
